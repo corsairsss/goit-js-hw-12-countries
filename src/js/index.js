c4965dd8d34a1efefe3bb09fe6053286929b87ac
Osaulenko Oleksandr <oom2212@gmail.com>
2020-04-20 15:24:27 +0300

    hw12

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,28 @@
+import notyfOptions from '../config/notyf-options.js'
 import '../css/style.css';
 import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
-import foo from './main.js';
+import fetchArticles from './fetchArticles.js';
 import { debounce } from "debounce";
 import itemCountry from '../templates/listCountry.hbs';
+import itemListCountry from '../templates/itemListCountry.hbs';
 // --------------------------------------
-const inp = document.querySelector('.js-input');
+const notyf = new Notyf(notyfOptions)
+const inputSearchCountry = document.querySelector('.js-input');
 const listCountries = document.querySelector('.js-list');
 const itemFindCountry = document.querySelector('.item');
-inp.addEventListener('input', debounce(boo, 500));
 
-const notyf = new Notyf({
-  duration: 5000,
-  position: {
-    x: 'center',
-    y: 'top',
-  },
-  types: [
-    {
-      type: 'error',
-      backgroundColor: 'red',
-      duration: 2000,
-      dismissible: true,
-    },
-    {
+inputSearchCountry.addEventListener('input', debounce(showResultSearchCountry, 500));
 
-      type: 'info',
-      backgroundColor: '#3f3e3e',
-      icon: false,
-      message: 'Too many matches found.Please enter a more specific query',
-      dismissible: true,
-      duration: 2500,
-
-    },
-
-  ],
-});
-
-function boo(e) {
+function showResultSearchCountry(e) {
   if (listCountries.children.length > 1) listCountries.innerHTML = '';
   const name = e.target.value;
 
-  foo.fetchArticles(name).then(data => {
-    if (data.length > 10) notyf.open({
-      type: 'info',
-    });
+  fetchArticles.fetchArticles(name).then(data => {
+    if (data.length > 10) notyf.open({ type: 'info', });
     if (data.length > 1 && data.length < 10) {
-      data.map(item => {
-        itemFindCountry.innerHTML = '';
-        listCountries.insertAdjacentHTML('beforeend', `<li class="list-item">${item.name}</li>`)
-      })
+      itemFindCountry.innerHTML = '';
+      listCountries.insertAdjacentHTML('beforeend', itemListCountry(data))
     }
     if (data.length === 1) {
       itemFindCountry.innerHTML = '';
@@ -59,6 +32,5 @@ function boo(e) {
       notyf.error('This country not Find')
     }
   });
-
-
 }
+
